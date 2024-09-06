527d857b8e350ee632ef57a1665aa822ddfe499e
jorda <jyang74@students.ubc.ca>
2024-09-05 19:00:13 -0700

    Final app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import getResponse from './openai';
+
+function Message(text, user=true, loader=false) {
+  this.text = text;
+  this.id = this.text + Math.random();
+  this.loader = loader;
+  this.user = user;
+
+  this.addToChat = function(list) {
+    const updated = [...list];
+
+    if (updated.length > 0 && updated[updated.length - 1].loader) {
+      const loaderMsg = updated.pop();
+      updated.push(this);
+      updated.push(loaderMsg);
+    } else {
+      updated.push(this);
+    }
+
+    return updated;
+  }
+}
 
 function App() {
-
-  //const [user] = useAuthState(auth);
-
   return (
     <div className="App">
       <header>
@@ -22,24 +41,49 @@ function App() {
 
 function ChatBot() {
   const dummy = useRef();
-  const [messages, addMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
   const [formValue, setFormValue] = useState('');
+  const [msgBuffer, setBuffer] = useState('');
 
-  const sendMessage = (e) => {
+  useEffect(() => {
+    if (dummy.current) {
+      dummy.current.scrollIntoView({behavior : "smooth"});
+    }
+  }, [messages]);
+
+
+  useEffect(() => {
+    if (msgBuffer.length > 0) {
+      const withoutLoader = messages.filter(msg => !msg.loader);
+      setMessages(new Message(msgBuffer, false).addToChat(withoutLoader));
+      setBuffer('');
+    }
+  }, [msgBuffer]);
+
+  const sendMessage = async (e) => {
     e.preventDefault();
-    const updated = [...messages];
-    updated.push(formValue);
-    addMessage(updated);
+    setFormValue('');
 
-    console.log(updated);
+    const temp = new Message(formValue, true).addToChat(messages);
+    
+    if (!containsLoader(temp)) {
+      setMessages(new Message("", false, true).addToChat(temp));
+    } else {
+      setMessages(temp);
+    }
 
     dummy.current.scrollIntoView({ behavior: 'smooth' });
+    const chatbot = await getResponse(formValue); //!!! TEST !!!
+
+    if (Object.keys(chatbot).length > 0) {
+      setBuffer(chatbot.msg); 
+    }
   }
 
   return (<>
     <main>
 
-      {messages && messages.map(msg => <ChatMessage key={msg + new Date(8.64e15).toString()} message={msg} />)}
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg.text} user={msg.user} loader={msg.loader}/>)}
 
       <span ref={dummy}></span>
 
@@ -51,7 +95,7 @@ function ChatBot() {
         setFormValue(e.target.value)
         }} placeholder="say something nice" />
 
-      <button type="submit" disabled={false}>🕊️</button>
+      <button type="submit">🕊️</button>
 
     </form>
   </>);
@@ -59,8 +103,21 @@ function ChatBot() {
 
 function ChatMessage(props) {
   const text = props.message;
+  const isUser = props.user;
+  const isLoader = props.loader;
 
-  const messageClass = false ? 'sent' : 'received';
+  const messageClass = isUser ? 'sent' : 'received';
+
+  if (isLoader) {
+    return (<>
+    <div className={`message ${messageClass}`}>
+    <img src={ 'https://i.pinimg.com/736x/ec/d7/fb/ecd7fb760f2c39b7c7cd71b3bbbd959c.jpg'} />
+      <p className='wrapper'>
+        <div className='loader'/>
+      </p>
+    </div>
+    </>);
+  }
 
   return (<>
     <div className={`message ${messageClass}`}>
@@ -69,5 +126,16 @@ function ChatMessage(props) {
     </div>
   </>)
 }
+
+function containsLoader(messages) {
+  for (let i of messages) {
+    if (i.loader) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 
 export default App;
---
+++ src/backend/server.js
@@ -0,0 +1,45 @@
+const {OpenAI} = require('openai');
+const Configuration = require('openai');
+const express = require("express");
+const cors = require("cors");
+
+
+// const openai = new OpenAI(new Configuration({
+//     apiKey: "sk-FXxS8tLaawlvYwcVUo4__BHE8SK60aAhO9gd10McbGT3BlbkFJfHEAr6hG38QPp3-4StX5XwgdERV_vvWJtD2pcpOZwA",
+// }));
+
+const openai = new OpenAI({
+    apiKey: "e55eaf80fe87416fa2878de9af87c184",
+    baseURL: "https://api.aimlapi.com"
+});
+
+const app = express();
+const port = 6969;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/data', async (req, res) => {
+    console.log(req.body.msg);
+    const msg = await onChatCompletion(req.body.msg);
+    res.json({msg : msg});
+});
+
+async function onChatCompletion(input) {
+    const chatCompletion = await openai.chat.completions.create({
+        model: "mistralai/Mistral-7B-Instruct-v0.2",
+        messages: [
+          { role: "system", content: "You are a therapist. Be emphathetic and helpful" },
+          { role: "user", content: input }
+        ],
+        temperature: 0.7,
+        max_tokens: 128,
+      });
+
+      return chatCompletion.choices[0].message.content;
+}
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port);
+})
